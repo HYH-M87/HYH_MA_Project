826e0edbe431f4e6e9cdec4302056bf9471eb86f
HyhLovelycode <huangyihong0303@gmail.com>
2024-06-28 16:51:17 +0800

    modify the experiment configure file

--- experiments/MA_Detection/hyh_ma_det_exp006/res50_cbam_dcn/run.py
+++ experiments/MA_Detection/hyh_ma_det_exp006/res50_cbam_dcn/run.py
@@ -8,4 +8,4 @@ _base_ = [
 ]
 
 # log file and exoeriment info dir
-work_dir = './9_logs/MA_Detection/hyh_ma_det_exp006/res50_cbam_ori/finetune2'
+work_dir = './logs/MA_Detection/hyh_ma_det_exp006/res50_cbam_dcn/finetune'
