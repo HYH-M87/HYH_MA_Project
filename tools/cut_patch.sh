55d643e34ded292de558453882e262f33551b269
HyhLovelycode <huangyihong0303@gmail.com>
2024-07-12 13:58:24 +0800

    Added a configuration file for the running script and modified the bash script

--- tools/cut_patch.sh
+++ tools/cut_patch.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-ORI_DATA=$1
-PROCESSED_DATA=$2
-PATCH_SIZE_H=$3
-PATCH_SIZE_W=$4
-OVERLAP_RATE=$5
-SPLIT=${6:- -1}
-DESCRIPE=${7:-""}
+# 读取配置文件
+CONFIG_FILE=$1
+ORI_DATA=$(jq -r '.ORI_DATA' $CONFIG_FILE)
+PROCESSED_DATA=$(jq -r '.PROCESSED_DATA' $CONFIG_FILE)
+PATCH_SIZE_H=$(jq -r '.PATCH_SIZE_H' $CONFIG_FILE)
+PATCH_SIZE_W=$(jq -r '.PATCH_SIZE_W' $CONFIG_FILE)
+OVERLAP_RATE=$(jq -r '.OVERLAP_RATE' $CONFIG_FILE)
+SPLIT=$(jq -r '.SPLIT' $CONFIG_FILE)
+DESCRIPE=$(jq -r '.DESCRIPE' $CONFIG_FILE)
+
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python $(dirname "$0")/data_preprocess/cut_patch_overlap.py \
-    $ORI_DATA \
-    $PROCESSED_DATA \
-    $PATCH_SIZE_H \
-    $PATCH_SIZE_W \
-    $OVERLAP_RATE \
-    $SPLIT\
-    --descripe $DESCRIPE\
+  $ORI_DATA \
+  $PROCESSED_DATA \
+  $PATCH_SIZE_H \
+  $PATCH_SIZE_W \
+  $OVERLAP_RATE \
+  $SPLIT \
+  --descripe $DESCRIPE
--- tools/model_eval.sh
+++ tools/model_eval.sh
@@ -1,41 +1,33 @@
 #!/usr/bin/env bash
 
-MODE=$1
-shift
+# 读取配置文件
+CONFIG_FILE=$1
 
-# 根据 MODE 的值决定传入的 pred 参数数量
-PRED=()
-if [ "$MODE" == "model" ]; then
-    PRED+=("$1")
-    shift
-    PRED+=("$1")
-    shift
-else
-    PRED+=("$1")
-    shift
-fi
+MODE=$(jq -r '.MODE' $CONFIG_FILE)
+PRED=$(jq -r '.PRED[]' $CONFIG_FILE)
+DATADIR=$(jq -r '.DATADIR' $CONFIG_FILE)
+PATCH_SIZE_H=$(jq -r '.PATCH_SIZE_H' $CONFIG_FILE)
+PATCH_SIZE_W=$(jq -r '.PATCH_SIZE_W' $CONFIG_FILE)
+OVERLAP_RATE=$(jq -r '.OVERLAP_RATE' $CONFIG_FILE)
+SPLIT=$(jq -r '.SPLIT' $CONFIG_FILE)
+SCORE=$(jq -r '.SCORE' $CONFIG_FILE)
+IOU=$(jq -r '.IOU' $CONFIG_FILE)
+SAVEDIR=$(jq -r '.SAVEDIR' $CONFIG_FILE)
+DESCRIPE=$(jq -r '.DESCRIPE' $CONFIG_FILE)
 
-# 捕获剩余的参数
-DATADIR=$1
-PATCH_SIZE_H=$2
-PATCH_SIZE_W=$3
-OVERLAP_RATE=$4
-SPLIT=$5
-SCORE=$6
-IOU=$7
-SAVEDIR=$8
-DESCRIPE=${9:-""}
+# 将PRED参数转换为数组
+PRED_ARR=($PRED)
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python $(dirname "$0")/model_eval/model_eval.py \
-    $MODE \
-    ${PRED[@]} \
-    $DATADIR\
-    $PATCH_SIZE_H\
-    $PATCH_SIZE_W\
-    $OVERLAP_RATE\
-    $SPLIT\
-    $SCORE\
-    $IOU\
-    $SAVEDIR\
-    --descripe $DESCRIPE\
+  $MODE \
+  "${PRED_ARR[@]}" \
+  $DATADIR \
+  $PATCH_SIZE_H \
+  $PATCH_SIZE_W \
+  $OVERLAP_RATE \
+  $SPLIT \
+  $SCORE \
+  $IOU \
+  $SAVEDIR \
+  --descripe $DESCRIPE
